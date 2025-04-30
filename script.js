455a11e1f3628b2b5cc8d58efb17886bf8665ac2
mukesh <mukeshchakkaravarthy31@gmail.com>
2025-04-30 12:23:52 +0530

    move project local to public

---
+++ script.js
@@ -0,0 +1,11 @@
+let myVariable="computer science"
+
+//string count
+console.log(myVariable.length)
+
+//get the specific char at string
+console.log(myVariable.charAt(0))
+
+//index of the first string
+console.log(myVariable.indexOf("e"))
+console.log(myVariable.lastIndexOf("e"))
